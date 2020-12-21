b1fdc66692ae74706fa483c96ddf61d90b31d73e
wang475293638 <475293638@qq.com>
2020-12-21 20:54:32 +0800

    12.21

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -9,10 +9,12 @@
       <li class="staff" v-for="(item, index) in staffList" :key="index">
         <div class="staffContent" v-for="(items, i) in item" :key="i">
           <div class="staffPhoto">
-            <img :src="items.avatar" alt />
+            <img v-if="items.avatar" :src="items.avatar" alt />
+            <img v-else :src="defaultAvatar" alt />
           </div>
           <div class="staffText">
             <div class="staffname">{{ items.name }}</div>
+            <div class="department">{{ items.title }}</div>
             <div class="department">
               {{
               items.departList
@@ -33,6 +35,7 @@ export default {
   components: {},
   data() {
     return {
+      defaultAvatar: require("../assets/default_avatar.png"),
       height: 0, //页面高度
       count: 0,
       staffList: [] //员工列表
@@ -43,7 +46,8 @@ export default {
       return {
         direction: 2,
         limitMoveNum: 2,
-        step: 0.4 // 数值越大速度滚动越快
+        step: 6, // 数值越大速度滚动越快
+        openWatch: true // 开启数据实时监控刷新dom
       };
     }
   },
@@ -58,7 +62,7 @@ export default {
   mounted() {
     this.getUserList();
     (this.height =
-      document.getElementById("seamless-warp2").offsetHeight - 20 + "px"),
+      document.getElementById("seamless-warp2").offsetHeight - 2 + "px"),
       this.refreshTable(); //窗体刷新
   },
   methods: {
@@ -84,14 +88,17 @@ export default {
         i += 2;
       }
       this.staffList = staffArr;
+      console.log(this.staffList)
     },
     //页面刷新
     refreshTable() {
       let that = this;
       window.onresize = () => {
         return (() => {
-          that.height =
-            document.getElementById("seamless-warp2").offsetHeight - 20 + "px";
+          if (document.getElementById("seamless-warp2")) {
+            that.height =
+              document.getElementById("seamless-warp2").offsetHeight - 2 + "px";
+          }
         })();
       };
     }
@@ -103,7 +110,7 @@ export default {
   overflow: hidden;
   width: 100%;
   height: 100%;
-  background: url("../assets/login_bg.jpg") center/cover no-repeat;
+  background: url("../assets/bg_img.jpg") center/cover no-repeat;
 }
 .scrollBox {
   display: flex;
@@ -117,38 +124,42 @@ export default {
   display: flex;
   flex-flow: column nowrap;
   justify-content: space-between;
-  width: 1.88rem;
+  width: 1.66rem;
   height: 100%;
   margin-right: 0.072917rem;
 }
 .staffContent {
   display: flex;
   flex-flow: column;
-  justify-content: space-between;
   align-items: center;
-  width: 1.88rem;
-  /* height: 2.2rem; */
+  width: 1.66rem;
   height: 49%;
-  padding: 0.1rem 0.104167rem 0.09rem;
+  padding: 0.1rem 0.1rem 0.09rem;
   box-sizing: border-box;
   background: #fff;
 }
 .staffPhoto {
-  width: 1.69rem;
-  height: 1.69rem;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  width: 1.5rem;
+  height: 1.5rem;
+  overflow: hidden;
 }
 .staffPhoto img {
   width: 100%;
-  height: 100%;
+  /* height: 100%; */
 }
 .staffText {
   text-align: center;
 }
 .staffname {
-  font-size: 0.104167rem;
+  font-size: .104167rem;
+  margin: 0.2rem 0 0.05rem;
+  color: #333;
+  font-weight: bold;
 }
 .department {
-  margin-top: 0.02rem;
   font-size: 0.072917rem;
   color: #888;
 }
